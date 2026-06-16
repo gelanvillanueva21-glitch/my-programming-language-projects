45694afc0f5ae564189fe66dfc6a4c23acdb91bf
Gelan <gelanvillanueva21@gmail.com>
2026-06-16 22:36:02 +0800

    I finished my calulcator inside my notepad project, I had to learn some new stuff from the scratch and understand what I code, and it finished

--- WebDev/JavaScriptDom/Project-Notes/app.js
+++ WebDev/JavaScriptDom/Project-Notes/app.js
@@ -2,6 +2,8 @@
 
 // VARIABLES
 
+const titleBtn = document.getElementById('title-button');
+const calculatorBox = document.getElementById('calculator-container');
 const textArea = document.getElementById('note-content');
 const searchBtn = document.getElementById('search-note');
 const addBtn = document.getElementById('add-note');
@@ -11,13 +13,10 @@ const windowSearch = document.getElementById('window-box-search');
 const windowTitle = document.getElementById('window-title-box');
 const windowDelete = document.getElementById('window-delete-confirm');
 let savedList = document.getElementById('saved-note-list');
-
 const searchInput = document.getElementById('search-note-title');
 const searchTitleBtn = document.getElementById('search');
-
 const titleInput = document.getElementById('add-notes-title');
 const confirmBtn = document.getElementById('confirm-button');
-
 let headingTitle = document.getElementById('heading-title');
 let deleteNoteBtn = document.querySelectorAll('.delete-btn');
 let activeNote = null;
@@ -55,6 +54,11 @@ notes.forEach(function(note) {
 });
 
 
+titleBtn.addEventListener('click', function(){
+    window.location.reload();
+})
+
+
 
 // WINDOW POP UP COMPONENTS
 
@@ -89,7 +93,14 @@ searchTitleBtn.addEventListener('click', function(){
     } else {
         const allNotes = document.querySelectorAll('.list');
         allNotes.forEach(function(note){
-            if (searchInput.value.toLowerCase() === note.innerText.toLowerCase()) {
+            if (searchInput.value.toLowerCase() === note.innerText.toLowerCase() && note.id.toLowerCase() === 'calculator') {
+                headingTitle.innerText = note.innerText.toUpperCase();
+                textArea.style.display = 'none';
+                calculatorBox.style.display =  'flex';
+
+                searchInput.value = '';
+                windowSearch.style.display = 'none';
+            } else if (searchInput.value.toLowerCase() === note.innerText.toLowerCase()) {
                 note.style.borderColor = 'blue';
                 note.style.borderStyle = 'solid';
                 searchInput.value = '';
@@ -118,13 +129,13 @@ confirmBtn.addEventListener('click', function(){
     if (titleInput.value === '') {
         alert('You must Enter a title word');
     } else {
-        var isExist = false;
+        var isExist = true;
         allNotes.forEach(function(note) {
             if (note.innerText.toLowerCase() === titleInput.value.toLowerCase()) {
-                isExist = true;
+                isExist = false;
             }
         });
-        if (!isExist) {
+        if (isExist) {
             let li = document.createElement('li');
             li.innerText = titleInput.value;
             li.classList.add('list');
@@ -155,12 +166,18 @@ window.addEventListener('click', function(e) {
 // ACTIVE NOTES
 
 
-
 window.addEventListener('click', function(e) {
 
     const allNotes = document.querySelectorAll('.list');
     allNotes.forEach(function(note) {
-        if (e.target === note) {
+
+        if (e.target.innerText.toLowerCase() === 'calculator') {
+            headingTitle.innerText = e.target.innerText.toUpperCase();
+            textArea.style.display = 'none';
+            calculatorBox.style.display =  'flex';
+        } else if (e.target === note) {
+            calculatorBox.style.display = 'none';
+            textArea.style.display = 'inline-block';
             headingTitle.innerText = note.innerText;
             textArea.value = note.dataset.content;
             activeNote = note;
@@ -250,8 +267,169 @@ confirmButton.addEventListener('click', function(){
 });
 
 
+// CALCULATOR
+
+// VARIABLE
 
 
+const displayCalculate = document.getElementById('display-input');
+
+function saveCalculate() {
+    let allCalculateHistory = document.querySelectorAll('.second-list');
+    let history = []
+
+    allCalculateHistory.forEach((x) => {
+        history.push(x.innerText);
+    });
+    localStorage.setItem('history', JSON.stringify(history));
+}
+
+function appendDisplay(input) {
+
+    displayCalculate.value += input;
+}
+
+function deleteNumber() {
+    let tempVar = displayCalculate.value;
+    displayCalculate.value = tempVar.slice(0, -1);
+}
+
+let calculateHistoryBox = document.getElementById('history-box');
 
 
+function calculate() {
+
+    let isTrue = true;
+    let expression = displayCalculate.value;
+
+    try {
+        const operation = {
+            '+': (a, b) => a + b,
+            '-': (a, b) => a - b,
+            'x': (a, b) => a * b,
+            '/': (a, b) => a / b
+        };
+        let tokens = [];
+        let tempList = []
+
+        for (let i = 0; i < expression.length; i++) {
+
+            if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].includes(expression[i])) {
+                tempList.push(expression[i]);
+                if (!['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].includes(expression[i+1])) {
+                    let result = tempList.join("");
+                    tempList = []
+                    tokens.push(result);
+                }
+            } else {
+                if (expression[i] === '-') {
+                    if (i === 0 || ['+', '-', 'x', '/'].includes(expression[i-1])) {
+                        let num = '-';
+                        i++;
+                        while (i < expression.length && ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'].includes(expression[i])) {
+                            num += expression[i];
+                            i++
+                        }
+                    tokens.push(num)
+                    i--;
+                    } else {
+                        tokens.push('-');
+                    }
+                } else {
+                    tokens.push(expression[i]);
+                }
+            }
+        }
+        console.log(tokens);
+        let result = [];
+
+        for (let i = 0; i < tokens.length; i++) {
+            if (tokens[i] === 'x' || tokens[i] === '/') {
+                let prevNum = parseFloat(result[result.length-1]);
+                let nextNum = parseFloat(tokens[i+1]);
+                let resultNum = operation[tokens[i]](prevNum, nextNum);
+                result[result.length-1] = resultNum;
+                i++;
+            } else {
+                result.push(tokens[i]);
+            }
+        }
+
+        let total = parseFloat(result[0])
+        for (let i = 1; i < result.length; i++) {
+            if (result[i] === '+' || result[i] === '-') {
+                let prevNum = total;
+                let nextNum = parseFloat(result[i+1]);
+                let resultNum = operation[result[i]](prevNum, nextNum);
+                total = resultNum;
+            }
+        }
+        displayCalculate.value = total;
+
+        if (displayCalculate.value === 'NaN') {
+            throw new Error;
+        }
+
+    } catch (error) {
+        isTrue = false;
+        displayCalculate.value = 'Syntax Error';
+    }
+
+    if (isTrue) {
+        let li = document.createElement('li');
+        li.innerText = `${expression} = ${displayCalculate.value}`;
+        li.classList.add('second-list');
+        calculateHistoryBox.appendChild(li);
+    }
+
+}
+
+calculatorBox.addEventListener('click', (e) => {
+
+    if(!e.target.matches('button'))
+        return;
+
+    if(e.target.innerText === '=') {
+        calculate();
+        return;
+    }
+
+    if (e.target.innerText === '⌫') {
+        return;
+    }
+
+    if (e.target.innerText === 'C') {
+        displayCalculate.value = '';
+        return;
+    }
+
+    if (displayCalculate.value === 'Syntax Error') {
+        displayCalculate.value = '';
+    }
+
+
+    appendDisplay(e.target.innerText);
+
+});
+
+// WINDOW UNLOAD AND LOAD
+
+window.addEventListener('beforeunload', () => {
+    saveCalculate();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    let history = JSON.parse(localStorage.getItem('history')) || [];
+
+
+    history.forEach(function(x) {
+        let li = document.createElement('li');
+        li.innerText = x;
+        li.classList.add('second-list');
+        calculateHistoryBox.appendChild(li);
+    });
+
+
+});
 
